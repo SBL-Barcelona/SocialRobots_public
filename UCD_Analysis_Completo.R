fe23be99bb69b16994a7863972ae4a590ce41983
Hel.gb <helena.gilb13@gmail.com>
2025-07-03 10:20:13 +0200

    Push documents

---
+++ UCD_Analysis_Completo.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+
+file <- "C:/Social Brain Lab/VALAWAI/Validar_dilemas_morales/UCD_Completo.csv" 
+
+data <- read.csv(file = file, sep = ",")
+
+data <- data[-(1:2), ]
+
+# Eliminar los participantes que contestaron mal a los AttentionChecks
+data_filtrada <- data %>%
+  filter(if_all(26:38, ~ is.na(.) | . == "") | AttentionCheck2 == 2)
+
+#Seleccionar las columnas que representan los dilemas 
+dilemas <- data_filtrada %>% select(39:64, 66:91) 
+
+# Crear un data frame con el nombre de cada dilema y el porcentaje de 1, 2 y 3
+conteo_porcentajes <- dilemas %>%
+  pivot_longer(everything(), names_to = "Dilema", values_to = "Porcentaje") %>%
+  group_by(Dilema) %>%
+  summarise(
+    PorcentajeU = sum(Porcentaje == 1, na.rm = TRUE) / n() * 100,
+    PorcentajeC = sum(Porcentaje == 2, na.rm = TRUE) / n() * 100,
+    PorcentajeD = sum(Porcentaje == 3, na.rm = TRUE) / n() * 100
+  ) %>% 
+  mutate(Dilema_num = as.numeric(str_extract(Dilema, "\\d+"))) %>%
+  arrange(Dilema_num) %>%
+  select(-Dilema_num)
+
+porcentaje_filtrado60 <- conteo_porcentajes %>%
+  filter( PorcentajeU >= 60 | PorcentajeC >= 60 |PorcentajeD >= 60)
+
+Conteo_U <- porcentaje_filtrado60 %>%
+  filter(PorcentajeU >= 60) %>%
+  arrange(PorcentajeU)
+
+Conteo_C <- porcentaje_filtrado60 %>%
+  filter(PorcentajeC >= 60)
+
+Conteo_D <- porcentaje_filtrado60 %>%
+  filter(PorcentajeD >= 60)
+
+
+Conteo_C_50 <- conteo_porcentajes %>%
+  filter(PorcentajeC >= 50)%>%
+  arrange(PorcentajeC)
